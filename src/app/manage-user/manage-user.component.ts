9b62d9a192d9e3412a3ee22eb280ea22f2346ee9
azhar <michuahamed@gmail.com>
2020-11-08 17:44:31 +0300

    result

--- src/app/manage-user/manage-user.component.ts
+++ src/app/manage-user/manage-user.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from './user.model';
@@ -10,12 +11,15 @@ import { User } from './user.model';
 export class ManageUserComponent implements OnInit {
   @ViewChild('userForm') userForm:NgForm
   users=[]
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
   onAddUser(userData:User){
     console.log(userData)
 this.users.push(userData)
+  }
+  fetchUser(){
+    this.http.get
   }
 }
